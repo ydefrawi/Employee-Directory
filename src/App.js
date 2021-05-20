449c94df7a5d4a8a29b5e0aa60aed27feb6632aa
Yousef Defrawi <76498767+ydefrawi@users.noreply.github.com>
2021-05-20 17:52:20 -0500

    added comments

--- src/App.js
+++ src/App.js
@@ -16,6 +16,9 @@ class App extends Component {
     quote:"Engage!"
   };
 
+//Sorts columns alphabetically.
+//toggles between ascending and descending. 
+//Called with an onClick
   onSort(event, sortKey) {
     let randomBoolean = Math.random() < 0.5;
     let randBool=randomBoolean;
@@ -33,9 +36,10 @@ class App extends Component {
     this.setState({ employees, sortArrow })
   }
 
-  // Map over this.state.friends and render a FriendCard component for each friend object
+
   render() {
     const {employees, searchField}=this.state;
+  // filters employees array to only include employees that match the searchField value
     const filteredEmployees = employees.filter((item)=>(
       item.name.toLowerCase().includes(searchField.toLowerCase()) || 
       item.occupation.toLowerCase().includes(searchField.toLowerCase()) ||
@@ -45,9 +49,9 @@ class App extends Component {
     return (
       <Wrapper>
         <Title/>
-        <NavBar 
-        handleChange={(e)=>this.setState({searchField: e.target.value})} 
-        />
+    
+        <NavBar handleChange={(e)=>this.setState({searchField: e.target.value})}/>
+
         <TableHeader>
           <tr>
             <th scope="col">#</th>
@@ -57,6 +61,8 @@ class App extends Component {
             <th scope="col" onClick={e => this.onSort(e, 'location')}><button className="button btn">Location<i className={this.state.sortArrow}></i></button></th>
             <th scope="col" onClick={e => this.onSort(e, 'email')}><button className="button btn">Email<i className={this.state.sortArrow}></i></button></th>
           </tr>
+
+ {/* Renders all employees that match the search input by mapping through filteredEmployees */}
           {filteredEmployees.map((employee) => (
             <TableBody
             key={employee.id}
--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -4,17 +4,11 @@ import "./style.css";
 const picard = require('picard-quotes');
 
 
-// let faded=true;
-
-// let randomQuote = picard.quote((quote) => {
-//   return quote;
-// });
-
-
 function Title() {
-
+//initializes quote to "Engage"
   const [quote, setQuote] = useState("Engage!")
 
+//hook for animation
   const { play, style, isPlaying } = useAnimateKeyframes({
     iterationCount: "infinite",
     direction: "alternate",
@@ -26,13 +20,12 @@ function Title() {
   });
 
 
-
+//renders a new quote to the virtual dom every 7 seconds
   useEffect(() => {
     setInterval(newQuote, 7000)
+  }, [])
 
-  }, [quote])
-
-
+//grabs a new quote from the picard node package
   const newQuote = async () => {
     let randomQuote = await picard.quote((result) => {
       return result.quote;
@@ -43,10 +36,8 @@ function Title() {
 
   return (
     <div className="hero fade-in-image">
-
       <div className="px-4 py-5 my-5 text-center">
         <img onClick={() => play(!isPlaying)} className="d-block mx-auto mb-4 emblem" style={style} src="https://png2.cleanpng.com/sh/b9cb074f6a4e81c7fafe5703ad20f630/L0KzQYm3V8A2N5DokJH0aYP2gLBuTgN1aaMyjORuaz32hLL5hvxmbaUyetNtZ3Wwc7F0jgVvcZRmjNH7LXnxg7bujvEue6Vmip99cnXuPcTqifVva5YyTdQ8ZUHldbPoWMZjPGUzSKU8MkWzQYe4VcMxP2o8SKUANki8SXB3jvc=/kisspng-star-trek-starfleet-badge-communicator-insegna-star-trek-science-5b3e1beba86b44.0332501615307970356899.png" alt="" width="72" height="57" />
-
 
         <Animate
           play start = {{
@@ -61,24 +52,17 @@ function Title() {
           <h1 className="title">Employee Directory</h1>
         </Animate>
 
-
         <div className="col-lg-6 mx-auto">
           <Animate
             play start={{ opacity: 0 }}
             end={{ opacity: 1 }}
             duration={3}
-            delay={0.3}
-          >
+            delay={0.3}>
             <h5 id="picardQuote">{quote}</h5>
-
             <h6 id="picard">-Jean-Luc Picard</h6>
           </Animate>
-
         </div>
       </div>
-
-
-
     </div>
   )
 }
