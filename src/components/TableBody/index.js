40ba0475ab45a6450654d60d64927fe529bd8e1a
Yousef Defrawi <76498767+ydefrawi@users.noreply.github.com>
2021-05-20 23:17:51 -0500

    added galaxy image to images directory

--- src/components/TableBody/index.js
+++ src/components/TableBody/index.js
@@ -3,11 +3,12 @@ import "./style.css";
 
 
 function TableBody (props) {
+  let dynamicURL=`https://www.google.com/search?q=${props.name}+Star+Trek`
     return (
     <tr>
       <th scope="row" id={props.id} >{props.id}</th>
       <td><img className="portraits" height = " 80px" src={props.image} alt={props.name} /></td>
-      <td>{props.name}</td>
+      <td><a href={dynamicURL}>{props.name}</a></td>
       <td>{props.occupation}</td>
       <td>{props.location}</td>
       <td>{props.email}</td>
