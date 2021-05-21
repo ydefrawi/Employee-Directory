fb1f085804bcb33a40bc1e5ff33b5dbd2a91887f
Yousef Defrawi <76498767+ydefrawi@users.noreply.github.com>
2021-05-21 00:42:38 -0500

    styling and responsiveness changes

--- src/components/TableBody/index.js
+++ src/components/TableBody/index.js
@@ -7,8 +7,8 @@ function TableBody (props) {
     return (
     <tr>
       <th scope="row" id={props.id} >{props.id}</th>
-      <td><img className="portraits" height = " 80px" src={props.image} alt={props.name} /></td>
-      <td><a href={dynamicURL}>{props.name}</a></td>
+      <td><img className="portraits" src={props.image} alt={props.name} /></td>
+      <td><a href={dynamicURL} rel="noreferrer"target="_blank">{props.name}</a></td>
       <td>{props.occupation}</td>
       <td>{props.location}</td>
       <td>{props.email}</td>
--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -6,16 +6,18 @@ const picard = require('picard-quotes');
 
 function Title() {
 //initializes quote to "Engage"
-  const [quote, setQuote] = useState("Engage!")
+  const [quote, setQuote] = useState("Make it so.")
 
 //hook for animation
   const { play, style, isPlaying } = useAnimateKeyframes({
-    iterationCount: "infinite",
+    iterationCount: "2",
     direction: "alternate",
-    duration: 20,
+    duration: 0.24,
     keyframes: [
-      "transform: rotateX(0) rotateY(0) rotateZ(0)",
-      "transform: rotateX(360deg) rotateY(360deg) rotateZ(360deg)"
+      "transform: scale(1)",
+      "transform: scale(1.4)",
+      "transform: scale(1)"
+    
     ]
   });
 
@@ -36,8 +38,8 @@ function Title() {
 
   return (
     <div className="hero fade-in-image">
-      <div className="px-4 py-5 my-5 text-center">
-        <img onClick={() => play(!isPlaying)} className="d-block mx-auto mb-4 emblem" style={style} src="https://png2.cleanpng.com/sh/b9cb074f6a4e81c7fafe5703ad20f630/L0KzQYm3V8A2N5DokJH0aYP2gLBuTgN1aaMyjORuaz32hLL5hvxmbaUyetNtZ3Wwc7F0jgVvcZRmjNH7LXnxg7bujvEue6Vmip99cnXuPcTqifVva5YyTdQ8ZUHldbPoWMZjPGUzSKU8MkWzQYe4VcMxP2o8SKUANki8SXB3jvc=/kisspng-star-trek-starfleet-badge-communicator-insegna-star-trek-science-5b3e1beba86b44.0332501615307970356899.png" alt="" width="72" height="57" />
+      <div className="px-4 py-2 my-2 text-center">
+        <img onClick={() => play(!isPlaying)} className="d-block mx-auto emblem" style={style} src="https://png2.cleanpng.com/sh/b9cb074f6a4e81c7fafe5703ad20f630/L0KzQYm3V8A2N5DokJH0aYP2gLBuTgN1aaMyjORuaz32hLL5hvxmbaUyetNtZ3Wwc7F0jgVvcZRmjNH7LXnxg7bujvEue6Vmip99cnXuPcTqifVva5YyTdQ8ZUHldbPoWMZjPGUzSKU8MkWzQYe4VcMxP2o8SKUANki8SXB3jvc=/kisspng-star-trek-starfleet-badge-communicator-insegna-star-trek-science-5b3e1beba86b44.0332501615307970356899.png" alt="" width="72" height="57" />
 
         <Animate
           play start = {{
@@ -58,8 +60,8 @@ function Title() {
             end={{ opacity: 1 }}
             duration={3}
             delay={0.3}>
-            <h5 id="picardQuote">{quote}</h5>
-            <h6 id="picard">-Jean-Luc Picard</h6>
+            <h5 id="picardQuote">"{quote}"</h5>
+            <h6 id="picard">Jean-Luc Picard</h6>
           </Animate>
         </div>
       </div>
